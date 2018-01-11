7578b65c651a3f7ceed56d4d664d8e99f93639be
jie-uncle <6030804@qq.com>
2018-01-11 09:45:53 +0800

    zhengdejin

---
+++ zhengdejin/js/index.js
@@ -0,0 +1,61 @@
+$(function(){
+	var index = 0;
+	var dist = 960;
+	var timer = null;
+	$(".arrow_right").click(function(){
+		clearInterval(timer);
+		var n=judge(false,index);
+		$(".banner").stop();
+		$(".banner").animate({left:-(dist*n)+'px'},2000,function(){
+			$(".banner_nav li").removeClass("current").eq(index).addClass("current");
+		});
+		
+		timer = setInterval(toLeft,4000);
+	});
+	
+	$(".arrow_left").click(function(){
+		clearInterval(timer);
+		var n=judge(true,index);
+		$(".banner").stop();
+		$(".banner").animate({left:-(dist*n)+'px'},2000,function(){
+		$(".banner_nav li").removeClass("current").eq(index).addClass("current");
+			
+		});
+		timer = setInterval(toLeft,4000);
+	});
+	
+	$(".banner_nav li").click(function(){
+		clearInterval(timer);
+		index = $(this).index();
+		$(".banner").animate({left:-(dist*index)+'px'},2000);
+		$(".banner_nav li").removeClass("current").eq(index).addClass("current");
+		timer = setInterval(toLeft,4000);
+	});
+	
+	clearInterval(timer);
+	timer = setInterval(toLeft,4000);
+	
+	function toLeft(){
+		$(".banner").animate({left:-(dist*judge(false,index))+'px'},2000,function(){
+			$(".banner_nav li").removeClass("current").eq(index).addClass("current");
+		});
+		
+	}
+	
+	function judge(isLeft,i){
+		if(isLeft){
+			if(index <= 0){
+				index = $(".banner img").length-1;
+			}else{
+				index = --i;
+			}
+		}else{
+			if(index >= $(".banner img").length-1){
+				index = 0;
+			}else{
+				index = ++i;
+			}
+		}
+		return index;
+	}
+})
